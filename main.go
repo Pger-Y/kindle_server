bc04d7251af1720d1262f3efdb917a604e41dcd7
UUU <uuu@UUUdeMacBook-Pro.local>
2018-10-20 17:16:03 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+type VxMessage struct {
+	ToUserName   string
+	FromUserName string
+	CreateTime   int
+	MsgType      string
+	Content      string
+	Event        string
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("Xweixin_pathX", func(c *gin.Context) {
+		var vx_req VxMessage
+		if err := c.ShouldBindXML(&vx_req); err != nil {
+			//c.XML(http.StatusOK,
+			log.Println("Parse request error", err)
+
+		}
+		var vx_resp VxMessage
+		vx_resp.ToUserName, vx_resp.FromUserName = vx_req.FromUserName, vx_req.ToUserName
+		vx_resp.Content = "We received you message:" + vx_req.Content
+		c.XML(http.StatusOK, vx_resp)
+	})
+	r.Run()
+}
