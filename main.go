198efe50f7e16d7a702d96e3720be4e4e4fb384f
UUU <uuu@UUUdeMacBook-Pro.local>
2018-10-20 17:39:28 +0800

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-type VxMessage struct {
+type Xml struct {
 	ToUserName   string
 	FromUserName string
 	CreateTime   int
@@ -19,14 +19,14 @@ type VxMessage struct {
 func main() {
 	r := gin.Default()
 	r.POST("Xweixin_pathX", func(c *gin.Context) {
-		var vx_req VxMessage
+		var vx_req Xml
 		if err := c.ShouldBindXML(&vx_req); err != nil {
 			//c.XML(http.StatusOK,
 			log.Println("Parse request error", err)
 
 		}
 		log.Println("Request:", vx_req)
-		var vx_resp VxMessage
+		var vx_resp Xml
 		vx_resp.ToUserName, vx_resp.FromUserName = vx_req.FromUserName, vx_req.ToUserName
 		vx_resp.MsgType = "text"
 		vx_resp.CreateTime = vx_req.CreateTime + 1
